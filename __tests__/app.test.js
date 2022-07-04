4d802d52690678c4aa3410ff55cae80450f71a9c
Janash37 <janash_37@hotmail.co.uk>
2022-07-04 12:27:22 +0100

    add testing for the /api/reviews/:review_id endpoint

---
+++ __tests__/app.test.js
@@ -0,0 +1,42 @@
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+const db = require("../db/connection");
+const request = require("supertest");
+const app = require("../app");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  db.end();
+});
+
+describe("/api", () => {
+  describe("GET /api/categories", () => {
+    test("returns status 200 when a successful get request is made", () => {
+      return request(app).get("/api/categories").expect(200);
+    });
+    test("returns status 200 and an array of category objects, with slug and description properties", () => {
+      return request(app)
+        .get("/api/categories")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.categories).toHaveLength(4);
+          body.categories.forEach((category) => {
+            expect(category).toEqual(
+              expect.objectContaining({
+                slug: expect.any(String),
+                description: expect.any(String),
+              })
+            );
+          });
+        });
+    });
+  });
+  describe("GET /api/reviews/:review_id", () => {
+    test("returns status 200 when a successful get request is made", () => {
+      return request(app).get("/api/review/1").expect(200);
+    });
+  });
+});
