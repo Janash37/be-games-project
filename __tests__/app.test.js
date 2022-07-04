ab54fb6016d5fc8950f784d672eef970f1cc67fc
Janash37 <janash_37@hotmail.co.uk>
2022-07-04 12:07:42 +0100

    add first endpoint GET /api/categories, set up testing suite and implement error handling

---
+++ __tests__/app.test.js
@@ -0,0 +1,35 @@
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+const db = require("../db/connection");
+const request = require("supertest");
+const app = require("../app");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  db.end();
+});
+
+describe("GET /api/categories", () => {
+  test("returns status 200 when a successful get request is made", () => {
+    return request(app).get("/api/categories").expect(200);
+  });
+  test("returns status 200 and an array of category objects, with slug and description properties", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.categories).toHaveLength(4);
+        body.categories.forEach((category) => {
+          expect(category).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
