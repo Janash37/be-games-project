cca6f94d0be52b51c122dd1625785730a10c8e2d
Janash37 <janash_37@hotmail.co.uk>
2022-07-07 15:17:03 +0100

    refactor connection file

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({
@@ -19,4 +19,4 @@ const config =
       }
     : {};
 
-module.exports = new Pool();
+// module.exports = new Pool();
